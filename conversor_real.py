2332348821dfe260f85d580dcce4e31fa844f4ae
Pedro Paulo <152125344+PPrefeito@users.noreply.github.com>
2025-09-02 10:14:58 -0300

    Add files via upload

---
+++ conversor_real.py
@@ -0,0 +1,8 @@
+#conversor de dólar para real
+# Atualizar o dólar antes de fazer o cálculo
+dolar = 5.49
+real = float(input('Insira quanto você quer converter em reais: '))
+
+dolar = real / dolar
+
+print('Você tem: $', dolar , 'dólares')
